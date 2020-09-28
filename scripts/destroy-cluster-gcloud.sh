54a0ec3665e17aa2d7086cb4ea22d701b5709951
Dennis Bell <dennis.bell@merapar.com>
2020-09-28 11:56:41 +0200

    fix on destroy

--- scripts/destroy-cluster-gcloud.sh
+++ scripts/destroy-cluster-gcloud.sh
@@ -10,10 +10,10 @@ gcloud container clusters delete ${NAME}-demo-ee --zone ${ZONE} -q
 # Remove DNS Records
 VAMP_IP_ADDRESS=$(gcloud compute addresses describe ${NAME}-vamp --region ${REGION} | grep address: | awk '{print $2}')
 VGA_IP_ADDRESS=$(gcloud compute addresses describe ${NAME}-vga --region ${REGION} | grep address: | awk '{print $2}')
-gcloud dns record-sets transaction start --zone="demo-vamp-cloud"
-gcloud dns record-sets transaction remove $VAMP_IP_ADDRESS --name="${NAME}.demo-ee.vamp.cloud" --ttl="300" --type="A" --zone="demo-vamp-cloud"
-gcloud dns record-sets transaction remove $VGA_IP_ADDRESS --name="*.${NAME}.demo-ee.vamp.cloud" --ttl="300" --type="A" --zone="demo-vamp-cloud"
-gcloud dns record-sets transaction execute --zone="demo-vamp-cloud"
+gcloud dns record-sets transaction start --zone="demo-ee-vamp-cloud"
+gcloud dns record-sets transaction remove $VAMP_IP_ADDRESS --name="${NAME}.demo-ee.vamp.cloud" --ttl="300" --type="A" --zone="demo-ee-vamp-cloud" --verbosity=info
+gcloud dns record-sets transaction remove $VGA_IP_ADDRESS --name="*.${NAME}.demo-ee.vamp.cloud" --ttl="300" --type="A" --zone="demo-ee-vamp-cloud" --verbosity=info
+gcloud dns record-sets transaction execute --zone="demo-ee-vamp-cloud"
 
 # Remove IP Addresses
 gcloud compute addresses delete ${NAME}-vamp --region ${REGION} -q
